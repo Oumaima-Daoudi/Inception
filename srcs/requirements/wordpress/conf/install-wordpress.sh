376f5f77e8547efdf33b3e5bd8d6be1104c715e5
Oumaima-Daoudi <daoudioumaima300@gmail.com>
2024-09-12 12:20:51 +0100

    Set up redis cache for WordPress website

--- srcs/requirements/wordpress/conf/install-wordpress.sh
+++ srcs/requirements/wordpress/conf/install-wordpress.sh
@@ -20,15 +20,22 @@ if ! wp core is-installed --path=/wordpress --allow-root; then
     echo "Installing WordPress..."
 
     # Install WordPress
-    wp core install --path=/wordpress --url="https://localhost" --title="docker-project" --admin_user="oumaima" --admin_password="ensa-1234" --admin_email="oumaimadaoudi13@gmail.com" --skip-email --allow-root
+    wp core install --path=/wordpress --url="https://localhost" --title="docker-project" --admin_user="${WORDPRESS_DB_USER}" --admin_password="${WORDPRESS_DB_PASSWORD}" --admin_email="oumaimadaoudi13@gmail.com" --skip-email --allow-root
 
     # Create additional users
     wp user create ziad daoudioumaima@gmail.com --role=subscriber --user_pass="ziad1234" --path=/wordpress --allow-root
+
 
     echo "WordPress installed successfully."
 else
     echo "WordPress is already installed."
 fi
+
+# Install and activate Redis Object Cache plugin
+wp plugin install redis-cache --activate --path=/wordpress --allow-root
+
+# Enable Redis cache
+wp redis enable --path=/wordpress --allow-root
 
 # Make sure PHP-FPM is started
 php-fpm7.4 -F
